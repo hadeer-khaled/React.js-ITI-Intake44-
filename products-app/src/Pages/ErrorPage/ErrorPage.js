843272e098970e2f732a245a3992c11c8d7c3e08
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-03-19 08:59:41 +0200

    add error page

---
+++ products-app/src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ErrorPage() {
+  return <p>the page not found</p>;
+}
+
+export default ErrorPage;
--- products-app/src/router.js
+++ products-app/src/router.js
@@ -1,14 +1,37 @@
 import { Outlet, createBrowserRouter } from "react-router-dom";
 import Products from "./Pages/Products/Products";
 import ProductDetails from "./Pages/ProductDetails/ProductDetails";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
+import Home from "./Pages/Home/Home";
+import Navbar from "./Components/Navbar";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+}
+
 const router = createBrowserRouter([
   {
-    path: "products",
-    element: <Products></Products>,
-  },
-  {
-    path: "products/:productID",
-    element: <ProductDetails></ProductDetails>,
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products></Products>,
+      },
+      {
+        path: "products/:productID",
+        element: <ProductDetails></ProductDetails>,
+      },
+    ],
   },
 ]);
 
